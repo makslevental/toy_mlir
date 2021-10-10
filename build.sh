84a2695728dfcd7481a10848eae11b9f808aece1
Maksim Levental <mlevental@fb.com>
2021-10-10 15:57:57 -0500

    third commit; ch2 working

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 cmake -DCMAKE_BUILD_TYPE=Debug -B/Users/mlevental/dev_projects/toy_mlir/cmake-build-debug \
  -DLLVM_DIR=/Users/mlevental/dev_projects/llvm-project/build/lib/cmake/llvm/ \
+ -DMLIR_DIR=/Users/mlevental/dev_projects/llvm-project/build/lib/cmake/mlir/ \
  -G Ninja -S/Users/mlevental/dev_projects/toy_mlir;
 cmake --build /Users/mlevental/dev_projects/toy_mlir/cmake-build-debug --target toy -- -j 12;
+./cmake-build-debug/toy main.toy -emit=ast 2> main.ast
+./cmake-build-debug/toy main.toy -emit=mlir -mlir-print-debuginfo 2> main.mlir
 
